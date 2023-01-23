04e6f1f4f355cf0f1d3e1d846d34d53c0c97c0e2
schinni2 <siva.subbu65@gmail.com>
2023-01-23 17:00:17 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var City, State = "Cypress", "California"
+
+func main() {
+    fmt.Println("My Name is Siva")
+    fmt.Println("My City is", City,".")
+    fmt.Println("My State is", State + ".")
+}
